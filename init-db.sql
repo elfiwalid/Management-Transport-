d32c140209152f816eb033417aaf15fdd19acd96
elabdioui <elabdiouihaitam@gmail.com>
2025-07-26 18:07:17 +0100

    eureka not hsowing on docker localhost

---
+++ init-db.sql
@@ -0,0 +1,5 @@
+-- Créer les bases de données
+CREATE DATABASE authentification;
+CREATE DATABASE service_assurance;
+CREATE DATABASE service_sinistre;
+CREATE DATABASE service_admin;
--- service-sinistre/src/main/java/com/pfa/service_sinistre/ServiceSinistreApplication.java
+++ service-sinistre/src/main/java/com/pfa/service_sinistre/ServiceSinistreApplication.java
@@ -3,7 +3,9 @@ package com.pfa.service_sinistre;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class ServiceSinistreApplication {
 
 	public static void main(String[] args) {
